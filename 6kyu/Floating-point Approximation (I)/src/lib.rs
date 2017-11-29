26ce781291b226d5dfb35d7f95a6dbb0f5459fab
AurevoirXavier <c.estlavie@icloud.com>
2017-11-30 01:32:37 +0800

    01:32/11/30/2017

--- 6kyu/Floating-point Approximation (I)/src/lib.rs
+++ 6kyu/Floating-point Approximation (I)/src/lib.rs
@@ -1,7 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+fn f(x: f64) -> f64 {
+    x / (1. + (1. + x).sqrt())
 }
+
+/*fn f(x: f64) -> f64 {
+    let ref mut root = (1. + x).sqrt().to_string().chars().collect::<Vec<char>>()[2..].to_vec();
+    let mut result = vec!['0', '.'];
+    result.append(root);
+    result.iter().collect::<String>().parse::<f64>().unwrap()
+}*/
