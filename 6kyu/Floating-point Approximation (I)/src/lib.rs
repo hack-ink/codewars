16b2a48f5f41a981320557262b7b64bdcc862be5
AurevoirXavier <c.estlavie@icloud.com>
2017-11-30 10:15:24 +0800

    10:15/11/30/2017

--- 6kyu/Floating-point Approximation (I)/src/lib.rs
+++ 6kyu/Floating-point Approximation (I)/src/lib.rs
@@ -3,8 +3,7 @@ fn f(x: f64) -> f64 {
 }
 
 /*fn f(x: f64) -> f64 {
-    let ref mut root = (1. + x).sqrt().to_string().chars().collect::<Vec<char>>()[2..].to_vec();
-    let mut result = vec!['0', '.'];
-    result.append(root);
-    result.iter().collect::<String>().parse::<f64>().unwrap()
+    let mut root = (1. + x).sqrt().to_string().chars().collect::<Vec<char>>();
+    root[0] = '0';
+    root.iter().collect::<String>().parse::<f64>().unwrap()
 }*/
