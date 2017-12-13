00976f035b289cb6442ce9aa944dfdeec5846953
AurevoirXavier <c.estlavie@icloud.com>
2017-12-13 21:51:23 +0800

    21:51/12/13/2017

--- 5kyu/Decimal to Factorial and Back/src/lib.rs
+++ 5kyu/Decimal to Factorial and Back/src/lib.rs
@@ -1,12 +1,26 @@
 use std::collections::HashMap;
 
+enum Status {
+    Code,
+    Decode,
+}
+
+fn init_fac(x: u64, status: Status) -> Vec<u64> {
+    match status {
+        Status::Code => (),
+        Status::Decode => (),
+    }
+    unimplemented!()
+}
+
 fn dec2_fact_string(nb: u64) -> String {
+    let fac = init_fac(nb, Status::Code);
     unimplemented!()
 }
 
 fn fact_string_2dec(s: String) -> u64 {
+    let fac = init_fac(s.len() as u64, Status::Decode);
     let alphabet: HashMap<char, u64> = (b'A'..b'Z' + 1).map(|c| c as char).zip(10..).collect();
-    println!("{:?}", alphabet);
     unimplemented!()
 }
 
