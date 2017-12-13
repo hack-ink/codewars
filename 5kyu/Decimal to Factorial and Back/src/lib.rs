8e57bfd4b0c8421e5049ade6a7c25c0fb098e610
AurevoirXavier <c.estlavie@icloud.com>
2017-12-13 21:43:32 +0800

    21:43/12/13/2017

--- 5kyu/Decimal to Factorial and Back/src/lib.rs
+++ 5kyu/Decimal to Factorial and Back/src/lib.rs
@@ -1,9 +1,16 @@
-fn dec2_fact_string(nb: u64) -> String {}
+use std::collections::HashMap;
+
+fn dec2_fact_string(nb: u64) -> String {
+    unimplemented!()
+}
 
 fn fact_string_2dec(s: String) -> u64 {
+    let alphabet: HashMap<char, u64> = (b'A'..b'Z' + 1).map(|c| c as char).zip(10..).collect();
+    println!("{:?}", alphabet);
     unimplemented!()
 }
 
 fn main() {
-    println!("{}", dec2_fact_string(2982));
+    //println!("{}", dec2_fact_string(2982));
+    println!("{}", fact_string_2dec("4041000".to_string()));
 }
