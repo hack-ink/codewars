a314502090c9d0c0c098b232f9a33ff668cbb945
AurevoirXavier <c.estlavie@icloud.com>
2017-12-24 17:26:55 +0800

    17:26/12/24/2017

---
+++ 5kyu/John and Ann sign up for Codewars/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn john(n: i32) -> Vec<i32> {
+    unimplemented!()
+}
+
+pub fn ann(n: i32) -> Vec<i32> {
+    unimplemented!()
+}
+
+pub fn sum_john(n: i32) -> i32 {
+    unimplemented!()
+}
+
+pub fn sum_ann(n: i32) -> i32 {
+    unimplemented!()
+}
---
+++ 5kyu/John and Ann sign up for Codewars/src/main.rs
@@ -0,0 +1,12 @@
+extern crate john_and_ann_sign_up_for_codewars;
+
+fn main() {
+    println!("{:?}", john_and_ann_sign_up_for_codewars::john(11));
+    println!("{:?}", john_and_ann_sign_up_for_codewars::john(14));
+    println!("{:?}", john_and_ann_sign_up_for_codewars::ann(6));
+    println!("{:?}", john_and_ann_sign_up_for_codewars::ann(15));
+    println!("{}", john_and_ann_sign_up_for_codewars::sum_john(75));
+    println!("{}", john_and_ann_sign_up_for_codewars::sum_john(78));
+    println!("{}", john_and_ann_sign_up_for_codewars::sum_ann(115));
+    println!("{}", john_and_ann_sign_up_for_codewars::sum_ann(150));
+}
