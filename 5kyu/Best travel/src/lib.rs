60fafc4602990768f9b1ee68aa69a963dbe4750d
AurevoirXavier <c.estlavie@icloud.com>
2017-12-24 23:14:43 +0800

    23:14/12/24/2017

--- 5kyu/Best travel/src/lib.rs
+++ 5kyu/Best travel/src/lib.rs
@@ -1,10 +1,3 @@
 fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {
     1
 }
-
-fn main() {
-    let ts = &vec![91, 73, 73, 85, 73, 81, 87];
-    println!("{}", choose_best_sum(230, 3, ts));
-    let ts = &vec![50, 55, 56, 57, 58];
-    println!("{}", choose_best_sum(163, 3, ts));
-}
--- 5kyu/Weird prime generator/src/lib.rs
+++ 5kyu/Weird prime generator/src/lib.rs
@@ -1,7 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+fn testing1(n: i64, exp: i64) -> () {
+    assert_eq!(count_ones(n), exp)
+}
+fn testing2(n: i64, exp: i64) -> () {
+    assert_eq!(max_pn(n), exp)
+}
+fn testing3(n: i64, exp: i64) -> () {
+    assert_eq!(an_over_average(n), exp)
+}
+
+#[test]
+fn returns_expected() {
+    testing1(1, 1);
+    testing1(10, 8);
+    testing1(100, 90);
+
+    testing2(1, 5);
+    testing2(5, 47);
+    testing2(7, 101);
+
+    testing3(5, 3);
+}
+
+fn count_ones(n: i64) -> i64 {
+    unimplemented!()
+}
+
+fn max_pn(n: i64) -> i64 {
+    unimplemented!()
+}
+
+fn an_over_average(n: i64) -> i64 {
+    unimplemented!()
 }
