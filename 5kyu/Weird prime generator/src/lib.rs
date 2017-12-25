fecef267f67911899d1cde92e812209b3c50cc97
AurevoirXavier <c.estlavie@icloud.com>
2017-12-25 22:45:03 +0800

    22:45/12/25/2017

--- 5kyu/Weird prime generator/src/lib.rs
+++ 5kyu/Weird prime generator/src/lib.rs
@@ -1,9 +1,27 @@
+fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 { a } else { gcd(b, a % b) }
+}
+
+fn count_ones(n: i64) -> i64 {
+    unimplemented!()
+}
+
+fn max_pn(n: i64) -> i64 {
+    unimplemented!()
+}
+
+fn an_over_average(n: i64) -> i64 {
+    unimplemented!()
+}
+
 fn testing1(n: i64, exp: i64) -> () {
     assert_eq!(count_ones(n), exp)
 }
+
 fn testing2(n: i64, exp: i64) -> () {
     assert_eq!(max_pn(n), exp)
 }
+
 fn testing3(n: i64, exp: i64) -> () {
     assert_eq!(an_over_average(n), exp)
 }
@@ -19,16 +37,4 @@ fn returns_expected() {
     testing2(7, 101);
 
     testing3(5, 3);
-}
-
-fn count_ones(n: i64) -> i64 {
-    unimplemented!()
-}
-
-fn max_pn(n: i64) -> i64 {
-    unimplemented!()
-}
-
-fn an_over_average(n: i64) -> i64 {
-    unimplemented!()
 }
