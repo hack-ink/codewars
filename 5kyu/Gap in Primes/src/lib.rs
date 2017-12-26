0fba471d1461ca6efdef6122df05f7360f81e041
AurevoirXavier <c.estlavie@icloud.com>
2017-12-26 19:57:54 +0800

    19:57/12/26/2017

--- 5kyu/Gap in Primes/src/lib.rs
+++ 5kyu/Gap in Primes/src/lib.rs
@@ -1,5 +1,21 @@
+fn is_prime(x: u64) -> bool {
+    match x {
+        2 | 3 => return true,
+        x if x % 6 != 1 && x % 6 != 5 => return false,
+        _ => (),
+    }
+    let mut i = 5;
+    while i <= (x as f64).sqrt() as u64 {
+        if x % i == 0 || x % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+    true
+}
+
 fn gap(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
-    
+
 }
 
 fn testing(g: i32, m: u64, n: u64, exp: Option<(u64, u64)>) -> () {
