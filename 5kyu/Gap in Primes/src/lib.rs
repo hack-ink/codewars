2a43b6e28c6909cc2ddddaf54d4984b95b2ad971
AurevoirXavier <c.estlavie@icloud.com>
2017-12-26 17:10:37 +0800

    17:10/12/26/2017

--- 5kyu/Gap in Primes/src/lib.rs
+++ 5kyu/Gap in Primes/src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+fn gap(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
+    // your code
+}
+
+fn testing(g: i32, m: u64, n: u64, exp: Option<(u64, u64)>) -> () {
+    assert_eq!(gap(g, m, n), exp)
+}
+
+#[test]
+fn basics_gap() {
+    testing(2,100,110, Some((101, 103)));
+    testing(4,100,110, Some((103, 107)));
+    testing(6,100,110, None);
+    testing(8,300,400, Some((359, 367)));
 }
