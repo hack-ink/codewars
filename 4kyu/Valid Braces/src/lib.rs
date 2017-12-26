a03dfab9ca8de533ab67d55b096316e70d36983c
AurevoirXavier <c.estlavie@icloud.com>
2017-12-26 20:55:36 +0800

    20:55/12/26/2017

--- 4kyu/Valid Braces/src/lib.rs
+++ 4kyu/Valid Braces/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+fn valid_braces(s: &str) -> bool {
+    false
+}
+
+#[test]
+fn basic_tests() {
+    assert!(valid_braces("()"));
+    assert!(!valid_braces("[(])"));
 }
