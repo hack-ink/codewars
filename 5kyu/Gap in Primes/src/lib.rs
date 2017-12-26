a7e131b0863f57e38d63d9857b72cb23fded15ba
AurevoirXavier <c.estlavie@icloud.com>
2017-12-26 20:20:23 +0800

    20:20/12/26/2017

--- 5kyu/Gap in Primes/src/lib.rs
+++ 5kyu/Gap in Primes/src/lib.rs
@@ -15,7 +15,14 @@ fn is_prime(x: u64) -> bool {
 }
 
 fn gap(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
-
+    let g = g as u64;
+    let primes: Vec<u64> = (m..n + 1).filter(|&x| is_prime(x)).collect();
+    if primes.len() > 1 {
+        for i in 1..primes.len() {
+            if primes[i] - primes[i - 1] == g { return Some((primes[i - 1], primes[i])); }
+        }
+    }
+    None
 }
 
 fn testing(g: i32, m: u64, n: u64, exp: Option<(u64, u64)>) -> () {
@@ -24,8 +31,8 @@ fn testing(g: i32, m: u64, n: u64, exp: Option<(u64, u64)>) -> () {
 
 #[test]
 fn basics_gap() {
-    testing(2,100,110, Some((101, 103)));
-    testing(4,100,110, Some((103, 107)));
-    testing(6,100,110, None);
-    testing(8,300,400, Some((359, 367)));
+    testing(2, 100, 110, Some((101, 103)));
+    testing(4, 100, 110, Some((103, 107)));
+    testing(6, 100, 110, None);
+    testing(8, 300, 400, Some((359, 367)));
 }
