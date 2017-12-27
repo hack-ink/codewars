e810a4af0a07dd1c93f2800571d44ba559aa5456
AurevoirXavier <c.estlavie@icloud.com>
2017-12-27 21:53:14 +0800

    21:53/12/27/2017

--- 5kyu/Fibo akin/src/lib.rs
+++ 5kyu/Fibo akin/src/lib.rs
@@ -1,13 +1,29 @@
-fn length_sup_uk(n: i32, k: i32) ->i32 {
-    unimplemented!()
+fn un(n: i32) -> Vec<i32> {
+    let mut un = vec![1, 1];
+    for i in 2..n as usize {
+        let next = un[i - un[i - 1] as usize] + un[i - un[i - 2] as usize];
+        un.push(next);
+    }
+    un
 }
-fn comp(n: i32) ->i32 {
-    unimplemented!()
+
+fn length_sup_uk(n: i32, k: i32) -> i32 {
+    un(n).into_iter().filter(|&x| x >= k).count() as i32
+}
+
+fn comp(n: i32) -> i32 {
+    let un = un(n);
+    let mut count = 0;
+    for i in 2..un.len() {
+        if un[i] < un[i - 1] { count += 1; }
+    }
+    count
 }
 
 fn dotest1(n: i32, k: i32, exp: i32) -> () {
     assert_eq!(length_sup_uk(n, k), exp)
 }
+
 fn dotest2(n: i32, exp: i32) -> () {
     assert_eq!(comp(n), exp)
 }
@@ -19,6 +35,7 @@ fn test_length_sup_uk() {
     dotest1(2941, 862, 1246);
     dotest1(3177, 573, 2047);
 }
+
 #[test]
 fn test_comp() {
     dotest2(74626, 37128);
