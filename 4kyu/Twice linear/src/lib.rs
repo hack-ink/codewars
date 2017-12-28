bd25d6fd3344b9d0dc1d4eee5245805065b8be66
AurevoirXavier <c.estlavie@icloud.com>
2017-12-28 14:04:35 +0800

    14:04/12/28/2017

--- 4kyu/Twice linear/src/lib.rs
+++ 4kyu/Twice linear/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+fn dbl_linear(n: u32) -> u32{
+    // your code
+}
+
+fn testing(n: u32, exp: u32) -> () {
+    assert_eq!(dbl_linear(n), exp)
+}
+
+#[test]
+fn basics_dbl_linear() {
+    testing(10, 22);
+    testing(20, 57);
+    testing(30, 91);
+    testing(50, 175);
+    testing(100, 447);
 }
