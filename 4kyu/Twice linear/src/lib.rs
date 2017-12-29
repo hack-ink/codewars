abd98cbbce1d7ccf3c03b2b1b6a0c2d84661dc49
AurevoirXavier <c.estlavie@icloud.com>
2017-12-29 11:34:32 +0800

    11:34/12/29/2017

--- 4kyu/Twice linear/src/lib.rs
+++ 4kyu/Twice linear/src/lib.rs
@@ -1,5 +1,13 @@
-fn dbl_linear(n: u32) -> u32{
-    // your code
+use std::collections::HashSet;
+fn dbl_linear(n: u32) -> u32 {
+    let mut v = vec![];
+    for i in 0..11 {
+        v.push(2 * i + 1);
+        v.push(3 * i + 1);
+    }
+    v.sort();
+    println!("{:?}", v);
+    0
 }
 
 fn testing(n: u32, exp: u32) -> () {
@@ -8,9 +16,9 @@ fn testing(n: u32, exp: u32) -> () {
 
 #[test]
 fn basics_dbl_linear() {
-    testing(10, 22);
+//    testing(10, 22);
     testing(20, 57);
-    testing(30, 91);
-    testing(50, 175);
-    testing(100, 447);
+//    testing(30, 91);
+//    testing(50, 175);
+//    testing(100, 447);
 }
