a3dc2f4aad7ad6802b0d53a139667b311abd3213
AurevoirXavier <c.estlavie@icloud.com>
2017-12-30 14:17:12 +0800

    14:17/12/30/2017

--- 4kyu/Twice linear/src/lib.rs
+++ 4kyu/Twice linear/src/lib.rs
@@ -2,14 +2,16 @@
 //
 //fn dbl_linear(n: u32) -> u32 {
 //    let mut x = 1;
-//    let mut q2 = VecDeque::new();
-//    let mut q3 = VecDeque::new();
+//    let mut y = VecDeque::new();
+//    let mut z = VecDeque::new();
 //    for _ in 0..n {
-//        q2.push_back(2 * x + 1);
-//        q3.push_back(3 * x + 1);
-//        x = std::cmp::min(q2[0], q3[0]);
-//        if x == q2[0] { x = q2.pop_front().unwrap(); }
-//        if x == q3[0] { x = q3.pop_front().unwrap(); }
+//        let y_ = 2 * x + 1;
+//        let z_ = y_ + x;
+//        y.push_back(y_);
+//        z.push_back(z_);
+//        x = std::cmp::min(y[0], z[0]);
+//        if x == y[0] { x = y.pop_front().unwrap(); }
+//        if x == z[0] { x = z.pop_front().unwrap(); }
 //    }
 //    x
 //}
