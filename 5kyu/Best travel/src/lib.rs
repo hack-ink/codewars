d71d32b701960ca399c2d723b753a061df17f38e
AurevoirXavier <c.estlavie@icloud.com>
2017-12-31 14:18:59 +0800

    14:18/12/31/2017

--- 5kyu/Best travel/src/lib.rs
+++ 5kyu/Best travel/src/lib.rs
@@ -1,4 +1,6 @@
-fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {}
+fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {
+
+}
 
 fn testing(t: i32, k: i32, ls: &Vec<i32>, exp: i32) -> () {
     assert_eq!(choose_best_sum(t, k, ls), exp)
