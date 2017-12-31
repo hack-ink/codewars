5e7217d2c81ce9e591e6e580e1d6403c093254a2
AurevoirXavier <c.estlavie@icloud.com>
2017-12-31 17:48:44 +0800

    17:48/12/31/2017

--- 5kyu/Best travel/src/lib.rs
+++ 5kyu/Best travel/src/lib.rs
@@ -1,5 +1,33 @@
-fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {
+fn swap_item(a: &mut Vec<i32>, a_ptr: usize, b: &mut Vec<i32>, b_ptr: usize) {
+    let tmp = a[a_ptr];
+    a[a_ptr] = b[b_ptr];
+    b[b_ptr] = tmp;
+}
 
+fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {
+    let k = k as usize;
+    let mut ls = ls.clone();
+    ls.sort();
+    let pivot = ls.len() - k as usize;
+    let mut d = ls[pivot..].to_vec();
+    let mut ls = ls[..pivot].to_vec();
+    ls.reverse();
+    ls.push(d[0]);
+    let edge = ls.len() - 1;
+    let mut dp1 = 0;
+    let mut lsp1 = 0;
+    let mut dp2 = 1;
+    let mut lsp2 = 0;
+    println!("{:?}, {:?}", d, ls);
+    loop {
+        if lsp1 == edge {
+            lsp1 = 0;
+        } else { lsp1 += 1; }
+        match d.iter().sum::<i32>() {
+            sum if sum > t => d[dp1] = ls[lsp1],
+            sum @ _ => return sum
+        }
+    }
 }
 
 fn testing(t: i32, k: i32, ls: &Vec<i32>, exp: i32) -> () {
