eec55a496589398f5a9dbde90d270ddd8d8fc7d2
AurevoirXavier <c.estlavie@icloud.com>
2018-01-01 14:00:46 +0800

    14:00/01/01/2018

--- 5kyu/Best travel/src/lib.rs
+++ 5kyu/Best travel/src/lib.rs
@@ -1,44 +1,27 @@
 fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {
-    if (ls.len() as i32) < k { return -1; }
-    let ls1;
-    let mut d;
-    let mut ls2;
-    {   // make ls1 immutable and drop useless variables.
-        let mut ls = ls.clone();
-        ls.sort();
-        let pivot = ls.len() - k as usize;
-        d = ls[pivot..].to_vec();
-        let mut ls1_ = ls[..pivot].to_vec();
-        ls1_.reverse();
-        ls2 = ls1_.clone();
-        ls1_.push(d[0]);
-        ls1 = ls1_;
-    }
-    ls2.push(d[1]);
-    let edge = ls1.len() - 1;
-    let mut d_p = 1;
-    let mut ls1_p = 0;
-    let mut ls2_p = 0;
+    let len = ls.len();
+    if (len as i32) < k { return -1; }
+    let mut d: Vec<i32> = vec![];
+    let mut v: Vec<usize> = vec![];
+    for i in 0..k { v.push(i); }
     loop {
-        println!("{:?}, {:?}, {:?}", d, ls1, ls2);
-        match d.iter().sum::<i32>() {
-            sum if sum > t => { println!("{}", sum);d[0] = ls1[ls1_p]}
-            sum @ _ => return sum
-        }
-        if ls1_p == edge {
-            ls1_p = 0;
-            d[d_p] = ls2[ls2_p];
-            if ls2_p == edge {
-                ls2_p = 0;
-                d_p += 1;
-                if d_p as i32 == k { return -1; }
-                ls2.pop();
-                ls2.push(d[d_p]);
-                d[d_p] = ls2[ls2_p];
+        println!("{:?}", v);
+        if v[0] == len as i32 {
+            for i in 1..len {
+                if v[i - 1] == len as i32 {
+                    v[i - 1] = 1;
+                    v[i] += 1;
+                    if i + 1 == len {
+                        if let Some(max) = d.into_iter().filter(|&d| d < t).max() {
+                            return max;
+                        } else { return -1; }
+                    }
+                    if v[i + 1] != len as i32 - 1 { break; }
+                }
             }
-            ls2_p += 1;
-        } else { ls1_p += 1; }
+        } else { v[0] += 1; }
     }
+    unimplemented!()
 }
 
 fn testing(t: i32, k: i32, ls: &Vec<i32>, exp: i32) -> () {
