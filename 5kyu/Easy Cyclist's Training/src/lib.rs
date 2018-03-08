da20ee5218989c3aead770cbb9c20750a343b472
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-08 23:21:25 +0800

    23:21/03/08/18

--- 5kyu/Easy Cyclist's Training/src/lib.rs
+++ 5kyu/Easy Cyclist's Training/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+fn temps(v0: i32, slope: i32, d_tot: i32) -> i32 {
+    // your code
+}
+
+fn dotest(v0: i32, slope: i32, d_tot: i32, exp: i32) -> () {
+    assert_eq!(exp, temps(v0, slope, d_tot))
+}
+
+#[test]
+fn basic_tests() {
+    dotest(30, 5, 30, 114);
+    dotest(30, 20, 30, -1);
+    dotest(30, 8, 20, 110);
+    dotest(30, 0, 5, 9);
+    dotest(50, 10, 25, 185);
 }
