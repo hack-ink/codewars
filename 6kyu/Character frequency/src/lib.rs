5b0de7b1481b2cd44e1b866326ff8904686c040f
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-08 21:13:28 +0800

    21:13/03/08/18

--- 6kyu/Character frequency/src/lib.rs
+++ 6kyu/Character frequency/src/lib.rs
@@ -1,7 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::collections::BTreeMap;
+
+fn letter_frequency(input: &str) -> BTreeMap<char, i32> {
+    unimplemented!();
+}
+
+#[test]
+fn simpleword() {
+    let answer: BTreeMap<char, i32> =
+        [('a', 2),
+            ('c', 1),
+            ('l', 1),
+            ('t', 1),
+            ('u', 1)]
+            .iter().cloned().collect();
+
+    assert_eq!(letter_frequency("actual"), answer);
+}
+
+#[test]
+fn sequence() {
+    let answer: BTreeMap<char, i32> =
+        [('a', 3),
+            ('b', 2),
+            ('f', 1),
+            ('p', 1),
+            ('s', 1),
+            ('t', 2),
+            ('u', 1),
+            ('x', 5)]
+            .iter().cloned().collect();
+
+    assert_eq!(letter_frequency("AaabBF UttsP xxxxx"), answer);
 }
