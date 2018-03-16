3cee8717e3322b7d51848844a3d57dbea1dc82f0
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-16 22:59:24 +0800

    22:59/03/16/18

--- 5kyu/■□ Pattern □■ - Zoom/src/main.rs
+++ 5kyu/■□ Pattern □■ - Zoom/src/main.rs
@@ -1,11 +1,34 @@
 fn zoom(n: i32) -> String {
-    let mut square = Vec::new();
-    let pattern = if ((n + 1) / 2) % 2 == 0 { '□' } else { '■' };
+    let n = n as usize;
+    let half_n = (n + 1) / 2;
+    let mut half_square: Vec<String> = vec![];
+    let mut outside = '□';
+    let mut inside = '■';
 
-    square.resize(n * n, pattern);
+    if half_n % 2 != 0 { std::mem::swap(&mut outside, &mut inside) };
 
-    println!("{:?}", square);
-    unimplemented!()
+    let side = std::iter::repeat(vec![outside, inside]).take(half_n)
+        .collect::<Vec<Vec<char>>>()
+        .concat();
+
+    for i in 0..half_n {
+        let left: String = side.iter().take(i).collect();
+        let mid = std::iter::repeat(outside)
+            .take(n - 2 * i)
+            .collect::<String>();
+
+        half_square.push([left.clone(), mid, left.chars().rev().collect()].concat());
+        std::mem::swap(&mut outside, &mut inside);
+    }
+
+    let square = [
+        half_square.clone(),
+        half_square.into_iter().rev().collect::<Vec<String>>()[1..].to_vec()
+    ].concat().join("\n");
+
+    println!("{}", square);
+
+    square
 }
 
 #[test]
