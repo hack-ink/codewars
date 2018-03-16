08358d24602d4c344d2a6b1eb38b52c57b7bd0d0
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-16 16:04:44 +0800

    16:04/03/16/18

--- 5kyu/■□ Pattern □■ : Zoom/src/main.rs
+++ 5kyu/■□ Pattern □■ : Zoom/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+fn zoom(n: i32) -> String {
+    unimplemented!()
+}
+
+#[test]
+fn basic_test_1() {
+    assert_eq!(zoom(1), "■");
+}
+
+#[test]
+fn basic_test_2() {
+    assert_eq!(zoom(3), "\
+□□□
+□■□
+□□□"
+    );
+}
+
+#[test]
+fn basic_test_3() {
+    assert_eq!(zoom(5), "\
+■■■■■
+■□□□■
+■□■□■
+■□□□■
+■■■■■"
+    );
+}
+
+#[test]
+fn basic_test_4() {
+    assert_eq!(zoom(7), "\
+□□□□□□□
+□■■■■■□
+□■□□□■□
+□■□■□■□
+□■□□□■□
+□■■■■■□
+□□□□□□□"
+    );
+}
+
+#[test]
+fn basic_test_5() {
+    assert_eq!(zoom(9), "\
+■■■■■■■■■
+■□□□□□□□■
+■□■■■■■□■
+■□■□□□■□■
+■□■□■□■□■
+■□■□□□■□■
+■□■■■■■□■
+■□□□□□□□■
+■■■■■■■■■"
+    );
 }
