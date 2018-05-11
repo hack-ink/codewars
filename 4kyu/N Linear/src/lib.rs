4f26b3439b9c56f012b13b22f89495ff10525ac5
Xavier Lau <c.estlavie@icloud.com>
2018-05-11 08:25:39 +0800

    08:25/05/11/18

--- 4kyu/N Linear/src/lib.rs
+++ 4kyu/N Linear/src/lib.rs
@@ -1,5 +1,39 @@
 fn n_linear(m: &[u32], n: usize) -> u32 {
+    if n == 0 { return 1; }
+    if n < m.len() { return m[n - 1] + 1; }
 
+    let mut groups: Vec<Vec<u32>> = m.iter().map(|&y| y + 1).collect();
+    let mut x = m.iter().min().unwrap() + 1;
+    let mut t = 1;
+
+    loop {
+        let len = x.to_string().len();
+
+        for (i, group) in groups.iter_mut().enumerate() {
+            if len + m[i].to_string().len() > 6 { continue; }
+
+            group.push(x * m[i] + 1);
+        }
+
+        let i = groups.iter()
+            .map(|v| v[0])
+            .enumerate()
+            .min_by_key(|&(_, x)| x)
+            .unwrap()
+            .0;
+
+        if x == groups[i][0] {
+            groups[i].remove(0);
+
+            continue;
+        }
+
+        x = groups[i].remove(0);
+
+        t += 1;
+
+        if n == t { return x; }
+    }
 }
 
 #[test]
