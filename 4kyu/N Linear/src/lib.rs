2967a323d59363bc8287bb9d3cf097479342c2cb
Xavier Lau <c.estlavie@icloud.com>
2018-05-11 12:33:17 +0800

    12:33/05/11/18

--- 4kyu/N Linear/src/lib.rs
+++ 4kyu/N Linear/src/lib.rs
@@ -1,50 +1,44 @@
 fn n_linear(m: &[u32], n: usize) -> u32 {
-    if n == 0 { return 1; }
-    if n < m.len() { return m[n - 1] + 1; }
+    let mut xs = vec![1];
+    let mut x_i: Vec<usize> = vec![0; m.len()];
+    let mut u: Vec<(u32, usize)> = m.iter()
+        .enumerate()
+        .map(|(i, &x)| (x + 1, i))
+        .collect();
 
-    let mut groups: Vec<Vec<u32>> = m.iter().map(|&y| y + 1).collect();
-    let mut x = m.iter().min().unwrap() + 1;
-    let mut t = 1;
+    let mut t = 0;
+    let mut x = 1;
 
     loop {
-        let len = x.to_string().len();
+        if t == n { return x; }
 
-        for (i, group) in groups.iter_mut().enumerate() {
-            if len + m[i].to_string().len() > 6 { continue; }
+        match u.iter().min().unwrap() {
+            &(next, _) if next != x => {
+                xs.push(next);
 
-            group.push(x * m[i] + 1);
+                x = next;
+                t += 1;
+            }
+            &(_, i) => {
+                x_i[i] += 1;
+                u[i].0 = m[i] * xs[x_i[i]] + 1;
+
+                continue;
+            }
         }
-
-        let i = groups.iter()
-            .map(|v| v[0])
-            .enumerate()
-            .min_by_key(|&(_, x)| x)
-            .unwrap()
-            .0;
-
-        if x == groups[i][0] {
-            groups[i].remove(0);
-
-            continue;
-        }
-
-        x = groups[i].remove(0);
-
-        t += 1;
-
-        if n == t { return x; }
     }
 }
 
 #[test]
 fn pair_test() {
-    assert_eq!(n_linear(&[2, 3], 10), 22);
-    assert_eq!(n_linear(&[3, 2], 10), 22);
-    assert_eq!(n_linear(&[3, 2], 234), 1339);
+//    assert_eq!(n_linear(&[2, 3], 10), 22);
+//    assert_eq!(n_linear(&[3, 2], 10), 22);
+//    assert_eq!(n_linear(&[3, 2], 234), 1339);
 }
 
 #[test]
 fn triplet_test() {
-    assert_eq!(n_linear(&[5, 7, 8], 10), 64);
-    assert_eq!(n_linear(&[5, 7, 8], 11), 65);
+    assert_eq!(n_linear(&[2, 3, 10000002], 2000000), 133028307)
+//    assert_eq!(n_linear(&[5, 7, 8], 10), 64);
+//    assert_eq!(n_linear(&[5, 7, 8], 11), 65);
 }
