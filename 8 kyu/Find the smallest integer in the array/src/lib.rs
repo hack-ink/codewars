a31a169069d6f4234e0263bf0fa5646257c6ca88
Xavier Lau <c.estlavie@icloud.com>
2018-09-10 22:06:51 +0800

    22:06/09/10/2018

--- 8 kyu/Find the smallest integer in the array/src/lib.rs
+++ 8 kyu/Find the smallest integer in the array/src/lib.rs
@@ -1,3 +1,9 @@
-lfn main() {
-    println!("Hello, world!");
+fn find_smallest_int(arr: &[i32]) -> i32 {
+    *arr.into_iter().min().unwrap()
+}
+
+#[test]
+fn sample_tests() {
+    assert_eq!(find_smallest_int(&[34, 15, 88, 2]), 2);
+    assert_eq!(find_smallest_int(&[34, -345, -1, 100]), -345);
 }
